2f5b160ea1aba18761c5f9c03a82dcc46bfe2eb4
Veer0x1 <balveer.singhrao.eee21@itbhu.ac.in>
2023-06-16 22:11:47 +0530

    layout server session

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -1,7 +1,9 @@
 import "@/styles/globals.css"
 import { Metadata } from "next"
+import { getServerSession } from "next-auth/next"
 
 import { siteConfig } from "@/config/site"
+import { authOptions } from "@/lib/auth"
 import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
 import { Toaster } from "@/components/ui/toaster"
@@ -28,11 +30,11 @@ export const metadata: Metadata = {
 
 interface RootLayoutProps {
   children: React.ReactNode
-  session: never
 }
 
 // @ts-ignore
-export default function RootLayout({ children, session }: RootLayoutProps) {
+export default async function RootLayout({ children }: RootLayoutProps) {
+  const session = await getServerSession(authOptions)
   return (
     <>
       <html lang="en" suppressHydrationWarning>
