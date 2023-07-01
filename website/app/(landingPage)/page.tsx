37e0b8744f46a5864dd121e9009e04b8f8a15b48
rahul-X01 <rahul.kumar.eee21@gmail.com>
2023-07-01 15:54:07 +0530

    checking securety

--- website/app/(landingPage)/page.tsx
+++ website/app/(landingPage)/page.tsx
@@ -5,7 +5,6 @@ import ContactUs from "@/components/landing-page/ContactUs"
 import Hero from "@/components/landing-page/Hero"
 
 const Page = () => {
-  console.log(process.env.GOOGLE_APPLICATION_CREDENTIALS)
   return (
     <>
       <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
